ac7dca56cf96b8bc96511306510cc8c11391568f
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-10-19 09:50:58 +0300

    add customisation for textField

--- CashBackApp/ViewControllers/LoginViewController.swift
+++ CashBackApp/ViewControllers/LoginViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var viewForButton: UIView!
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    var bottomLine = CALayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         viewForButton.layer.cornerRadius = 30
         viewForButton.clipsToBounds = true
+        
+        bottomLine.frame = CGRect(x: 0.0, y: numberTextField.frame.height - 1, width: numberTextField.frame.width - 15, height: 1.0)
+        bottomLine.backgroundColor = UIColor.gray.cgColor
+        numberTextField.borderStyle = UITextField.BorderStyle.none
+        numberTextField.layer.addSublayer(bottomLine)
+
     }
     
 
