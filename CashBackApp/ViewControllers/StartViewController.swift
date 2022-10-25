4c5d6c8afe5d6d958602b0333040f7c691add4f6
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-10-25 09:16:41 +0300

    remove table view from startVC and add login button

--- CashBackApp/ViewControllers/StartViewController.swift
+++ CashBackApp/ViewControllers/StartViewController.swift
@@ -10,23 +10,17 @@ import UIKit
 class StartViewController: UIViewController {
 
     @IBOutlet weak var searchBarView: UIView!
+    @IBOutlet weak var loginButton: UIButton!
     
-    
-    @IBOutlet weak var marketsTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-       
-        
-//        marketsTableView.frame = CGRect(x: searchBarView.frame.minX, y: searchBarView.frame.maxY + 10, width: view.frame.minX, height: view.frame.maxY)
-        marketsTableView.delegate = self
-        marketsTableView.dataSource = self
-        marketsTableView.register(UINib(nibName: "MainScreenMarketsTableViewCell", bundle: nil), forCellReuseIdentifier: MainScreenMarketsTableViewCell.key)
         
         searchBarView.layer.cornerRadius = 32
         searchBarView.clipsToBounds = true
         
-//        view.addSubview(marketsTableView)
+        loginButton.layer.cornerRadius = 15
+        loginButton.clipsToBounds = true
+
     }
 
     @IBAction func searchButtonPressed(_ sender: Any) {
@@ -37,18 +31,3 @@ class StartViewController: UIViewController {
 
 }
 
-extension StartViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Магазины"
-    }
-    
-    
-}
